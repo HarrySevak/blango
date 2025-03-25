1b135b926ec96666ac9f379023124ded7ca2ca03
Hariom Sevak <hariom.sevak@birlasoft.com>
2025-03-25 14:36:58 +0000

    Generic relationship completed

--- blog2/admin.py
+++ blog2/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from blog2.models import Tag, Post
+from blog2.models import Tag, Post, Comment
 # Register your models here.
 
-
-admin.site.register(Tag)
 
 class PostAdmin(admin.ModelAdmin):
   prepopulated_fields ={"slug":("title",)}
 
   list_display = ('summary','content')
 
+
+admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
